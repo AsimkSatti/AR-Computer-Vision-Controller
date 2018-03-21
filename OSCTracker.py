acd5aa098e62b54825e5b31f92835d17a44b6b47
AsimkSatti <aks618@nyu.edu>
2018-03-21 05:22:10 -0400

    Update OSCTracker.py

--- OSCTracker.py
+++ OSCTracker.py
@@ -9,7 +9,7 @@ import time
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
-
+#A UDP connection to Unity with OSC; running locally at port 8050
 parser = argparse.ArgumentParser()
 parser.add_argument("--ip", default="127.0.0.1", help="The ip of the OSC server")
 parser.add_argument("--port", type=int, default=8050, help="The port the OSC server is listening on")
